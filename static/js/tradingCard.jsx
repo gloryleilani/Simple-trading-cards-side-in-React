c2c0008cfb5b03e2e0cb182283e26c115a6c0e8f
Glory Ludwick <lani.ludwick@gmail.com>
2020-08-04 08:44:40 -0700

    Building simple trading card site in React

---
+++ static/js/tradingCard.jsx
@@ -0,0 +1,76 @@
+var tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  }
+];
+
+function TradingCard(props) {
+  return (
+    <div className="card">
+      <p>Name: {props.name}</p>
+      <img src={props.imgUrl} onError={addDefaultSource} />
+      <p>Skill: {props.skill} </p>
+    </div>
+  );
+}
+
+const addDefaultSource = (evt) => {
+  evt.target.src = '/static/img/placeholder.png';
+}
+
+function TradingCardContainer() {
+  
+  const floatCard = {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+
+  };
+
+  const [cards, updateCards] = React.useState([floatCard]); 
+
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then ((response) => response.json())
+    .then((data) => updateCards(data))
+
+  }, []);
+
+
+  const tradingCards = [];
+
+  for (const currentCard of cards) {
+    tradingCards.push(
+           
+      <TradingCard
+        key={currentCard.name}
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl= {currentCard.imgUrl}
+        />
+      
+);}
+
+  return (
+    <div>{tradingCards}</div>
+  );
+}
+
+ReactDOM.render(
+  <TradingCardContainer />,
+  document.getElementById('container')
+);
